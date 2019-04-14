af0b3a0ce40dff2c95069ea919b2de85bc0eaf6c
aLesiuk <AndrewLesiuk359@gmail.com>
2019-04-14 18:05:17 +0300

    Easybot try 1

--- players.py
+++ players.py
@@ -37,7 +37,8 @@ class EasyBot(GameParticipants, Bot):
         self.name = "[Easy Bot] " + Bot.choose_name(0, 18)
     def specify_coordinate(self, enemy_field):
         attempt = 1
-        while (attempt<3) :
+        r=3
+        while (attempt<5) :
             pix = random.randint(0, 9)
             piy = random.randint(0, 9)
             for i in range(10):
@@ -87,21 +88,31 @@ class EasyBot(GameParticipants, Bot):
             if (enemy_field.map[pix][piy]==1):
                 return (pix, piy)
             elif(enemy_field.map[pix][piy]==0):
-                if ((j == 0)or(j == 1)or(j == 2)or(i == 0)or(i == 1)or(i == 2)):
-                    attempt = attempt + 1
-                else:
-                    i = pix-2
-                    j = piy-2
-                    for i in range(5):
-                        for j in range(5):
-                            if (enemy_field.map[pix][piy]==1):
-                                return (pix, piy)
+                xmax= pix+r
+                ymax= piy+r
+                xmin= pix-r
+                ymin = piy - r
+                if (pix+r>9):
+                    xmax= 9
+                if(piy+r>9):
+                    ymin = 9
+                if (pix - r < 0):
+                    xmax= 0
+                if (pix - r < 0):
+                    ymax = 0
+
+                for k in range(xmin+1,xmax):
+                    for h in range(ymin+1,ymax):
+                        if (enemy_field.map[k][h]==1):
+                            return (pix, piy)
             elif((enemy_field.map[pix][piy]==5)or(enemy_field.map[pix][piy]==4)):
                 attempt=attempt
             else:
                 attempt = attempt +1
-        return(random.randint(0, 9), random.randint(0, 9))
-        #hi
+        for i in range(10):
+            for j in range(10):
+                if(enemy_field.map[i][j] == 1):
+                    return(i, j)
 
 class NormalBot(GameParticipants, Bot):
     def __init__(self):
